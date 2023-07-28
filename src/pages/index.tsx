e11aaf97b501d9811a86b5844d304b0f8132d994
Piyush <bhangalepiyush@gmail.com>
2023-07-28 23:25:05 +0530

    feat: auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,35 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useEffect } from "react";
-import { api } from "~/utils/api";
+import { match } from "ts-pattern";
 
 export default function Home() {
   const session = useSession();
 
   useEffect(() => {
-    console.log(session)
-  }, [session])
+    console.log(session);
+  }, [session]);
 
   return (
     <div>
-      <h1>Auth</h1>
-      <button className="bg-red-400 p-8" onClick={() => void signIn("github")}>Sign in</button>
+      {match(session)
+        .with({ status: "authenticated" }, () => (
+          <div>
+            <h1>Hello {session.data!.user.name}</h1>
+            <button className="bg-red-400 p-8" onClick={() => void signOut()}>
+              Sign out
+            </button>
+          </div>
+        ))
+        .with({ status: "unauthenticated" }, () => (
+          <button
+            className="bg-blue-400 p-8"
+            onClick={() => void signIn("github")}
+          >
+            Sign in
+          </button>
+        ))
+        .with({ status: "loading" }, () => <h1>Loading</h1>)
+        .exhaustive()}
     </div>
   );
 }
