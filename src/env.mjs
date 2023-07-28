a9d2621102fe9b445b0d1314e9cd5ff1e6755ced
Piyush <bhangalepiyush@gmail.com>
2023-07-28 23:15:47 +0530

    feat: setup auth

--- src/env.mjs
+++ src/env.mjs
@@ -24,8 +24,8 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    GITHUB_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    GITHUB_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   },
 
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
